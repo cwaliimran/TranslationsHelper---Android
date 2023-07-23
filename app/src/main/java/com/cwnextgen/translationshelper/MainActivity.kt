9f3caba0fb2091b3d0ec1ee9105350e52d3fe493
Ali Imran <cwaliimran@gmail.com>
2023-07-23 21:49:12 +0500

    Translations helper initial commit

---
+++ app/src/main/java/com/cwnextgen/translationshelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cwnextgen.translationshelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
